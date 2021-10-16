45990178aa9863f67915d57bfc85e17b3c2ddbb3
Timothé Gélibert <tim.gelibert@protonmail.com>
2021-10-16 18:21:53 +0200

    Created serializers

---
+++ softdesk/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import User, Project, Issue, Contributors, Comments
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+
+class IssueSerializer(ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = "__all__"
+
+
+class ProjectSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = "__all__"
+
+
+class CommentsSerializer(ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = "__all__"
+
+
+class ContributorsSerializer(ModelSerializer):
+    class Meta:
+        model = Contributors
+        fields = "__all__"
